cc584fd16cd359f83ca096de075412b46c523114
Kevin Son <kevinson@Kevins-MacBook-Air.local>
2020-08-26 21:03:00 -0700

    adding more DOM

--- main.js
+++ main.js
@@ -1,4 +1,4 @@
-let myLibrary = ["fuck"];
+let myLibrary = [new Book("fuck", "fuck", "fuck", "fuck")];
 
 function Book(title, author, pages, read) {
     this.title = title;
@@ -14,17 +14,39 @@ function addBookToLibrary() {
 
 function render() {
     var body = document.querySelector('#container');
-    for (currBook in myLibrary) {
+    for (currBookIndex in myLibrary) {
+        const currBook = myLibrary[currBookIndex];
+
+        // creating a new card to be displayed
         var currCard = document.createElement("div");
         currCard.classList.add("card");
         var descriptionCard = document.createElement("div");
         descriptionCard.classList.add("description");
-        var author = document.createElement("h4");
+
+        var bookTitle = document.createElement("h3");
+        bookTitle.id = "title";
+        bookTitle.textContent = currBook.title;
+
+        var author = document.createElement("p");
         author.textContent = currBook.author;
-        descriptionCard.appendChild
+
+        var pages = document.createElement("p");
+        pages.textContent = currBook.pages;
+
+        var read = document.createElement("p");
+        read.textContent = currBook.read;
+        
+        descriptionCard.appendChild(bookTitle);
+        descriptionCard.appendChild(document.createElement("hr"));
+        descriptionCard.appendChild(author);
+        descriptionCard.appendChild(pages);
+        descriptionCard.appendChild(read);
         currCard.appendChild(descriptionCard);
         body.appendChild(currCard);
     }
 }
+
+var newBookButton = document.querySelector("#nbbutton");
+newBookButton.addEventListener("click", () => {})
 
 render();
